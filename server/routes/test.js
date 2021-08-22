21fdd95c9a3c8e0c0b182a3d628fbe4af564edf7
Andy Kan <andy.kmc@hotmail.com>
2021-08-22 19:09:27 +0100

    Added server

---
+++ server/routes/test.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const fetch = require('node-fetch');
+const router = express.Router();
+
+/* GET home page. */
+router.get('/postcode', async (req, res, next) => {
+  const { q } = req.query;
+  const resposne = await fetch(
+    `https://uk.api.just-eat.io/restaurants/bypostcode/${q}`
+  );
+  const data = await resposne.json();
+  res.json(data);
+});
+
+module.exports = router;
